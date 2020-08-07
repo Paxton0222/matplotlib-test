fbfbcadadf6830efc021a0a6141b6fdf9c47f748
Paxton0222 <paxton900222@gmail.com>
2020-08-07 14:23:28 +0800

    2

---
+++ test-matplotlib2.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+import numpy as np
+mpl.rcParams['lines.linewidth'] = 5
+mpl.rcParams['lines.color'] = 'r'
+mpl.rcParams['figure.figsize'] = (10, 10)
+plt.gcf().set_size_inches(10, 10)
+
+x = pd.period_range(pd.datetime.now(), periods=200, freq='d')
+x = x.to_timestamp().to_pydatetime()
+# 產生三組，每組 200 個隨機常態分布元素
+y = np.random.randn(200, 3).cumsum(0)
+plt.plot(x, y)
+plt.show()
